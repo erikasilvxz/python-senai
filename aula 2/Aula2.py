0901799710ede9d13506343fa1ac0e130a1a5fb5
erikasilvxz <erikkassantos557@gmail.com>
2024-12-03 11:32:30 -0300

    add variavel

---
+++ aula 2/Aula2.py
@@ -0,0 +1,11 @@
+print('bom dia a todos')
+
+nome_usuario='erika'# tipo-> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.64 # tipo -> float= ponto flutuante
+maior_idade= False # tipo -> bool = boolean
+
+print('seja bem vindo', nome_usuario)
+print('sua idade e', idade_usuario, 'anos')
+print('voce tem',  altura_usuario, 'metros')
+print('voce e maior de idade', maior_idade)
